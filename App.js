c93d2f63d503bb6ed72b14d893cf259d0520f428
Davoleo <davoleo2001@gmail.com>
2019-08-21 11:20:48 +0200

    Controlling Component Dimensions

--- App.js
+++ App.js
@@ -3,18 +3,23 @@ import { StyleSheet, Text, View } from 'react-native';
 import ImageBase from './components/ImageBase';
 import Greeting from "./components/Greeting"
 import BlinkText from './components/BlinkingText';
+import DimensionsSample from './components/DimensionsSample';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
-      <ImageBase></ImageBase>
+      {/* <ImageBase></ImageBase> */}
       <Greeting name="Davoleo"></Greeting>
       <Greeting name="Gianpaolo"></Greeting>
-      <BlinkText text="I'm some simple blinking text" />
+      {/* <BlinkText text="I'm some simple blinking text" />
       <BlinkText text="It's not that I'm blinking for you or anything ù.ù" />
-      <BlinkText text="Blink Blink Blink" />
+      <BlinkText text="Blink Blink Blink" /> */}
+    
+      <DimensionsSample fix={true}></DimensionsSample>    
     </View>
+    
+    //<DimensionsSample fix={false}></DimensionsSample>  //Everything else must be commented for this example to work
   );
 }
 
